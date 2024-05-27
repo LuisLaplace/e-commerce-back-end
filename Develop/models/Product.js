ada2cba4c9ee1d81482fa0bf2190148ee2a3cabe
Laplace <lap2837@outlook.com>
2024-05-27 16:42:01 -0600

    Defined columns and added the association code for each under the models folder

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true, // This will ensure the value is a decimal
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10, // Set the default value to 10
+      validate: {
+        isInt: true // Validate that the value is an integer (numeric)
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "Category", // Reference to the Category model
+        key: 'id'        // Column name in the Category model
+      }
+    }
   },
   {
     sequelize,
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.associate = (models) => {
+  Product.belongsTo(models.Category, { foreignKey: 'category_id' });
+};
 
 // Categories have many Products
-
+Category.associate = (models) => {
+  Category.hasMany(models.Product, { foreignKey: 'category_id' });
+};
 // Products belongToMany Tags (through ProductTag)
-
+Product.associate = (models) => {
+  Product.belongsTo(models.Category, { foreignKey: 'category_id' });
+  Product.belongsToMany(models.Tag, { through: models.ProductTag, foreignKey: 'product_id' });
+};
 // Tags belongToMany Products (through ProductTag)
+Tag.associate = (models) => {
+  Tag.belongsToMany(models.Product, { through: models.ProductTag, foreignKey: 'tag_id' });
+};
 
 module.exports = {
   Product,
